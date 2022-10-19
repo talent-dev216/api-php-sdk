1db2e3a76de497277ce3a11fea9a4df0fcc90a96
talent-dev216 <talent.dev0216@gmail.com>
2022-10-19 11:02:03 +0300

    preparing an example "get a card type" #ONBOARDING-7

---
+++ Examples/get_a_card_type.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Get a card type
+ */
+
+use CrmCareCloud\Webservice\RestApi\Client\ApiException;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\CareCloud;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\Config;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\Data\AuthTypes;
+
+require_once '../vendor/autoload.php';
+
+$project_uri     = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
+$login           = 'login';
+$password        = 'password';
+$external_app_id = 'application_id';
+$auth_type       = AuthTypes::BEARER_AUTH;
+// Or if using basic auth, just change the AuthType to Basic Auth
+// $authType      = AuthTypes::BASIC_AUTH;
+
+$config = new Config($project_uri, $login, $password, $external_app_id, $auth_type);
+
+$care_cloud = new CareCloud($config);
+
+// Set Header parameter Accept-Language
+$accept_language = 'en'; //	string | The unique id of the language code by ISO 639-1 Default: cs, en-gb;q=0.8
+
+// Set path parameter
+$card_type_id = '8bed991c68a470e7aaeffbf048'; // string | The unique id of the card type
+
+// Call endpoint and get data
+try {
+    $get_card_type = $care_cloud->cardTypesApi()->getCardType($card_type_id);
+    $card_type = $get_card_type->getData();
+} catch (ApiException $e) {
+    die(var_dump($e->getResponseBody() ?: $e->getMessage()));
+}
