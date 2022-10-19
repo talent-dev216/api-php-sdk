09615413f1b6db4df59eb05ed6da73b9971f64a3
talent-dev216 <talent.dev0216@gmail.com>
2022-10-19 12:52:30 +0300

    preparing an example "get a campaign product store records" #ONBOARDING-7

---
+++ Examples/get_a_campaign_product_store_records.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Get a campaign product store records
+ */
+
+use CrmCareCloud\Webservice\RestApi\Client\ApiException;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\CareCloud;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\Config;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\Data\AuthTypes;
+
+require_once '../vendor/autoload.php';
+
+$project_uri     = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
+$login           = 'login';
+$password        = 'password';
+$external_app_id = 'application_id';
+$auth_type      = AuthTypes::BEARER_AUTH;
+// Or if using basic auth, just change the AuthType to Basic Auth
+// $authType      = AuthTypes::BASIC_AUTH;
+
+$config = new Config($project_uri, $login, $password, $external_app_id, $auth_type);
+
+$care_cloud = new CareCloud($config);
+
+// Set Header parameter Accept-Language
+$accept_language = 'en'; //	string | The unique id of the language code by ISO 639-1 Default: cs, en-gb;q=0.8
+
+// Set path parameter
+$campaign_product_id = '8bed991c68a470e7aaeffbf048'; // string | The unique id of the campaign product
+
+// Set query parameters
+$count          = 10; // integer >= 1 | The number of records to return (optional, default is 100)
+$offset         = 0; // integer | The number of records from a collection to skip (optional, default is 0)
+$sort_field     = null; // string | One of the query string parameters for sorting (optional, default is null)
+$sort_direction = 'DESC'; // string | Direction of sorting the response list (optional, default is null)
+
+// Call endpoint and get data
+try {
+    $get_campaign_product_store_records = $care_cloud->campaignProductsApi()->getCampaignProductStoreRecords(
+        $campaign_product_id,
+        $accept_language,
+        $count,
+        $offset,
+        $sort_field,
+        $sort_direction
+    );
+    $campaign_product_store_records = $get_campaign_product_store_records->getData()->getCampaignProductStoreRecords();
+    $total_items = $get_campaign_product_store_records->getData()->getTotalItems();
+} catch (ApiException $e) {
+    die(var_dump($e->getResponseBody() ?: $e->getMessage()));
+}
