591875264eaf24065b8885a15ade91967b745c8f
talent-dev216 <talent.dev0216@gmail.com>
2022-10-19 17:21:49 +0300

    updating an example "get all customer vouchers" #ONBOARDING-8

--- Examples/get_all_customer_vouchers.php
+++ Examples/get_all_customer_vouchers.php
@@ -33,10 +33,10 @@ $count          = 1; // integer >= 1 | The number of records to return (optional
 $offset         = 0; // integer | The number of records from a collection to skip (optional, default is 0)
 $sort_field     = null; // string | One of the query string parameters for sorting (optional, default is null)
 $sort_direction = 'DESC'; // string | Direction of sorting the response list (optional, default is null)
-$code  = null; // string | The unique code of the voucher (optional, default is null)
-$store_id = null; // string | The unique id of the store where customer can apply the reward (optional, default is null)
-$is_valid = null; // boolean | In validity range - true / before or after validity range - false / no value - all (optional, default is null)
-$is_applied = null; // boolean | Filter by voucher application and reservation (optional, default is null)
+$code           = null; // string | The unique code of the voucher (optional, default is null)
+$store_id       = null; // string | The unique id of the store where customer can apply the reward (optional, default is null)
+$is_valid       = null; // boolean | In validity range - true / before or after validity range - false / no value - all (optional, default is null)
+$is_applied     = null; // boolean | Filter by voucher application and reservation (optional, default is null)
 $without_stores = null; // boolean | If true, the data will not contain information about business units (stores). If false, or not set resource returns default structure (optional)
 
 // Call endpoint and get data
