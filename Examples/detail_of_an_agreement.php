f5b2b8a80d3577681ecbb5e60d668b2a349c396b
talent-dev216 <talent.dev0216@gmail.com>
2022-10-21 14:13:55 +0300

    formatting an example "detail of an agreement" #ONBOARDING-7

--- Examples/detail_of_an_agreement.php
+++ Examples/detail_of_an_agreement.php
@@ -11,11 +11,11 @@ use CrmCareCloud\Webservice\RestApi\Client\SDK\Data\AuthTypes;
 
 require_once '../vendor/autoload.php';
 
-$project_uri     = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
-$login           = 'login';
-$password        = 'password';
+$project_uri = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
+$login = 'login';
+$password = 'password';
 $external_app_id = 'application_id';
-$auth_type      = AuthTypes::BEARER_AUTH;
+$auth_type = AuthTypes::BEARER_AUTH;
 // Or if using basic auth, just change the AuthType to Basic Auth
 // $authType      = AuthTypes::BASIC_AUTH;
 
@@ -35,7 +35,7 @@ try
     $get_agreement = $care_cloud->agreementsApi()->getAgreement($agreement_id, $accept_language);
     $agreement = $get_agreement->getData();
 }
-catch (ApiException $e)
+catch(ApiException $e)
 {
     die(var_dump($e->getResponseBody() ?: $e->getMessage()));
 }
