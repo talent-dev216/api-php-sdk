6133caba1c40f5a982235325722785c947590965
talent-dev216 <talent.dev0216@gmail.com>
2022-10-21 14:45:25 +0300

    formatting an example "get all countries" #ONBOARDING-7

--- Examples/get_all_countries.php
+++ Examples/get_all_countries.php
@@ -10,11 +10,11 @@ use CrmCareCloud\Webservice\RestApi\Client\SDK\Data\AuthTypes;
 
 require_once '../vendor/autoload.php';
 
-$project_uri     = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
-$login           = 'login';
-$password        = 'password';
+$project_uri = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
+$login = 'login';
+$password = 'password';
 $external_app_id = 'application_id';
-$auth_type      = AuthTypes::BEARER_AUTH;
+$auth_type = AuthTypes::BEARER_AUTH;
 // Or if using basic auth, just change the AuthType to Basic Auth
 // $authType      = AuthTypes::BASIC_AUTH;
 
@@ -26,10 +26,13 @@ $care_cloud = new CareCloud($config);
 $accept_language = 'en'; //	string | The unique id of the language code by ISO 639-1 Default: cs, en-gb;q=0.8
 
 // Call endpoint and get data
-try {
+try
+{
     $get_countries = $care_cloud->countriesApi()->getCountries($accept_language);
     $countries = $get_countries->getData()->getCountries();
     $total_items = $get_countries->getData()->getTotalItems();
-} catch (ApiException $e) {
+}
+catch(ApiException $e)
+{
     die(var_dump($e->getResponseBody() ?: $e->getMessage()));
 }
