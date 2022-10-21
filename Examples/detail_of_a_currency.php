b74729b777f4f64f7e0c4390dbd1a6b3fcf6b7e9
talent-dev216 <talent.dev0216@gmail.com>
2022-10-21 14:47:17 +0300

    formatting an example "detail of a currency" #ONBOARDING-7

--- Examples/detail_of_a_currency.php
+++ Examples/detail_of_a_currency.php
@@ -10,11 +10,11 @@ use CrmCareCloud\Webservice\RestApi\Client\SDK\Data\AuthTypes;
 
 require_once '../vendor/autoload.php';
 
-$project_uri     = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
-$login           = 'login';
-$password        = 'password';
+$project_uri = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
+$login = 'login';
+$password = 'password';
 $external_app_id = 'application_id';
-$auth_type      = AuthTypes::BEARER_AUTH;
+$auth_type = AuthTypes::BEARER_AUTH;
 // Or if using basic auth, just change the AuthType to Basic Auth
 // $authType      = AuthTypes::BASIC_AUTH;
 
@@ -29,9 +29,12 @@ $accept_language = 'en'; //	string | The unique id of the language code by ISO 6
 $currency_id = '8bed991c68a470e7aaeffbf048'; // string | The unique id of the currency
 
 // Call endpoint and get data
-try {
+try
+{
     $get_currency = $care_cloud->currenciesApi()->getCurrency($currency_id, $accept_language);
     $currency = $get_currency->getData();
-} catch (ApiException $e) {
+}
+catch(ApiException $e)
+{
     die(var_dump($e->getResponseBody() ?: $e->getMessage()));
 }
