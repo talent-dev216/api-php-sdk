5f1fcdebb32fe70c01dda864e3f8d27d225c9f25
talent-dev216 <talent.dev0216@gmail.com>
2022-10-21 14:33:48 +0300

    formatting an example "get all cards" #ONBOARDING-7

--- Examples/get_all_cards.php
+++ Examples/get_all_cards.php
@@ -10,11 +10,11 @@ use CrmCareCloud\Webservice\RestApi\Client\SDK\Data\AuthTypes;
 
 require_once '../vendor/autoload.php';
 
-$project_uri     = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
-$login           = 'login';
-$password        = 'password';
+$project_uri = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
+$login = 'login';
+$password = 'password';
 $external_app_id = 'application_id';
-$auth_type       = AuthTypes::BEARER_AUTH;
+$auth_type = AuthTypes::BEARER_AUTH;
 // Or if using basic auth, just change the AuthType to Basic Auth
 // $authType      = AuthTypes::BASIC_AUTH;
 
@@ -26,18 +26,19 @@ $care_cloud = new CareCloud($config);
 $accept_language = 'en'; //	string | The unique id of the language code by ISO 639-1 Default: cs, en-gb;q=0.8
 
 // Set query parameters
-$count          = 10; // integer >= 1 | The number of records to return (optional, default is 100)
-$offset         = 0; // integer | The number of records from a collection to skip (optional, default is 0)
-$sort_field     = null; // string | One of the query string parameters for sorting (optional, default is null)
+$count = 10; // integer >= 1 | The number of records to return (optional, default is 100)
+$offset = 0; // integer | The number of records from a collection to skip (optional, default is 0)
+$sort_field = null; // string | One of the query string parameters for sorting (optional, default is null)
 $sort_direction = 'DESC'; // string | Direction of sorting the response list (optional, default is null)
-$customer_id    = null; // string | The unique id of the customer (optional, default is null)
-$card_number    = null; // string | Number of the customer card (optional, default is null)
-$card_type_id   = null; // string | The unique id of the card type (optional, default is null)
-$state          = null; // integer | State of the customer. Possible values are: 0 - deleted / 1 - active / 2 - non active (optional, default is null)
-$is_valid       = null; // boolean | in validity range - true / before or after validity range - false / no value - all (optional, default is null)
+$customer_id = null; // string | The unique id of the customer (optional, default is null)
+$card_number = null; // string | Number of the customer card (optional, default is null)
+$card_type_id = null; // string | The unique id of the card type (optional, default is null)
+$state = null; // integer | State of the customer. Possible values are: 0 - deleted / 1 - active / 2 - non active (optional, default is null)
+$is_valid = null; // boolean | in validity range - true / before or after validity range - false / no value - all (optional, default is null)
 
 // Call endpoint and get data
-try {
+try
+{
     $get_cards = $care_cloud->cardsApi()->getCards(
         $accept_language,
         $count,
@@ -52,6 +53,8 @@ try {
     );
     $cards = $get_cards->getData()->getCards();
     $total_items = $get_cards->getData()->getTotalItems();
-} catch (ApiException $e) {
+}
+catch(ApiException $e)
+{
     die(var_dump($e->getResponseBody() ?: $e->getMessage()));
 }
