9591a8bd2af9e9dab8c98ebf7d2fb08689ccfa6e
talent-dev216 <talent.dev0216@gmail.com>
2022-10-21 14:43:21 +0300

    formatting an example "get info about all card types" #ONBOARDING-7

--- Examples/get_info_about_all_card_types.php
+++ Examples/get_info_about_all_card_types.php
@@ -10,11 +10,11 @@ use CrmCareCloud\Webservice\RestApi\Client\SDK\Data\AuthTypes;
 
 require_once '../vendor/autoload.php';
 
-$project_uri     = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
-$login           = 'login';
-$password        = 'password';
+$project_uri = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
+$login = 'login';
+$password = 'password';
 $external_app_id = 'application_id';
-$auth_type       = AuthTypes::BEARER_AUTH;
+$auth_type = AuthTypes::BEARER_AUTH;
 // Or if using basic auth, just change the AuthType to Basic Auth
 // $authType      = AuthTypes::BASIC_AUTH;
 
@@ -26,17 +26,20 @@ $care_cloud = new CareCloud($config);
 $accept_language = 'en'; //	string | The unique id of the language code by ISO 639-1 Default: cs, en-gb;q=0.8
 
 // Set query parameters
-$count          = 10; // integer >= 1 | The number of records to return (optional, default is 100)
-$offset         = 0; // integer | The number of records from a collection to skip (optional, default is 0)
-$sort_field     = null; // string | One of the query string parameters for sorting (optional, default is null)
+$count = 10; // integer >= 1 | The number of records to return (optional, default is 100)
+$offset = 0; // integer | The number of records from a collection to skip (optional, default is 0)
+$sort_field = null; // string | One of the query string parameters for sorting (optional, default is null)
 $sort_direction = 'DESC'; // string | Direction of sorting the response list (optional, default is null)
-$name           = null; // string | Search record by name or a part of the name (optional, default is null)
+$name = null; // string | Search record by name or a part of the name (optional, default is null)
 
 // Call endpoint and get data
-try {
+try
+{
     $get_card_types = $care_cloud->cardTypesApi()->getCardTypes();
     $card_types = $get_card_types->getData()->getCardTypes();
     $total_items = $get_card_types->getData()->getTotalItems();
-} catch (ApiException $e) {
+}
+catch(ApiException $e)
+{
     die(var_dump($e->getResponseBody() ?: $e->getMessage()));
 }
