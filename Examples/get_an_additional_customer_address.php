f1ea90d5de1e174d01a3c0c4e9decc53d559b9ac
talent-dev216 <talent.dev0216@gmail.com>
2022-10-21 11:41:24 +0300

    preparing an example "get_an_additional_customer_address" #ONBOARDING-8

---
+++ Examples/get_an_additional_customer_address.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Get an additional customer address
+ */
+
+use CrmCareCloud\Webservice\RestApi\Client\ApiException;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\CareCloud;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\Config;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\Data\AuthTypes;
+
+require_once '../vendor/autoload.php';
+
+$project_uri = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
+$login = 'login';
+$password = 'password';
+$external_app_id = 'application_id';
+$auth_type = AuthTypes::BEARER_AUTH;
+// Or if using basic auth, just change the AuthType to Basic Auth
+// $authType      = AuthTypes::BASIC_AUTH;
+
+$config = new Config($project_uri, $login, $password, $external_app_id, $auth_type);
+
+$care_cloud = new CareCloud($config);
+
+// Set Header parameter Accept-Language
+$accept_language = 'en'; //	string | The unique id of the language code by ISO 639-1 Default: cs, en-gb;q=0.8
+
+// Set path parameter
+$customer_id = '8ea2591121e636086a4a9c0992'; // string | The unique id of the customer
+$additional_customer_address_id_path = '86e05affc7a7abefcd513ab400'; // string | Unique ID of the customer additional address
+
+// Call endpoint and get data
+try
+{
+    $get_address = $care_cloud->customersApi()->getSubCustomerAddress($customer_id, $additional_customer_address_id_path, $accept_language);
+    $address = $get_address->getData()->getAddress();
+    $address_type = $get_address->getData()->getAddressType();
+}
+catch(ApiException $e)
+{
+    die(var_dump($e->getResponseBody() ?: $e->getMessage()));
+}
