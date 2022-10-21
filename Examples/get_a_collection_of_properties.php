cd0101d0ecae49007f3120fcf06b0921bab75219
talent-dev216 <talent.dev0216@gmail.com>
2022-10-21 15:09:16 +0300

    formatting an example "get a collection of properties" #ONBOARDING-8

--- Examples/get_a_collection_of_properties.php
+++ Examples/get_a_collection_of_properties.php
@@ -10,11 +10,11 @@ use CrmCareCloud\Webservice\RestApi\Client\SDK\Data\AuthTypes;
 
 require_once '../vendor/autoload.php';
 
-$project_uri     = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
-$login           = 'login';
-$password        = 'password';
+$project_uri = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
+$login = 'login';
+$password = 'password';
 $external_app_id = 'application_id';
-$auth_type       = AuthTypes::BEARER_AUTH;
+$auth_type = AuthTypes::BEARER_AUTH;
 // Or if using basic auth, just change the AuthType to Basic Auth
 // $authType      = AuthTypes::BASIC_AUTH;
 
@@ -29,10 +29,13 @@ $accept_language = 'en'; //	string | The unique id of the language code by ISO 6
 $customer_id = '8ea2591121e636086a4a9c0992'; // string | The unique id of the customer
 
 // Call endpoint and get data
-try {
+try
+{
     $get_properties = $care_cloud->customersApi()->getSubCustomerProperties($customer_id, $accept_language);
     $properties = $get_properties->getData()->getPropertyRecords();
     $total_items = $get_properties->getData()->getTotalItems();
-} catch (ApiException $e) {
+}
+catch(ApiException $e)
+{
     die(var_dump($e->getResponseBody() ?: $e->getMessage()));
 }
