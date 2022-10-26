c9888739e9b6a0613746c186cdcfb2765023dd75
talent-dev216 <talent.dev0216@gmail.com>
2022-10-26 17:24:12 +0300

    preparing an example "get a product group" #ONBOARDING-15

---
+++ Examples/get_a_product_group.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Get a product group
+ */
+
+use CrmCareCloud\Webservice\RestApi\Client\ApiException;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\CareCloud;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\Config;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\Data\AuthTypes;
+
+require_once '../vendor/autoload.php';
+
+$project_uri = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
+$login = 'login';
+$password = 'password';
+$external_app_id = 'application_id';
+$auth_type = AuthTypes::BEARER_AUTH;
+// Or if using basic auth, just change the AuthType to Basic Auth
+// $authType      = AuthTypes::BASIC_AUTH;
+
+$config = new Config($project_uri, $login, $password, $external_app_id, $auth_type);
+
+$care_cloud = new CareCloud($config);
+
+// Set Header parameter Accept-Language
+$accept_language = 'en'; //	string | The unique id of the language code by ISO 639-1 Default: cs, en-gb;q=0.8
+
+// Set path parameters
+$product_group_id = '8bf3397ab62d3ffd876fc9ae16'; // string | The unique id of the product group
+
+// Call endpoint and get data
+try
+{
+    $get_product_group = $care_cloud->productGroupsApi()->getProductGroup($product_group_id, $accept_language);
+    $product_group = $get_product_group->getData();
+}
+catch(ApiException $e)
+{
+    die(var_dump($e->getResponseBody() ?: $e->getMessage()));
+}
