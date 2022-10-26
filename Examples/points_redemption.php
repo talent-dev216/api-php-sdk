5195ee888a5f52450b70e6aad2b21ba929328241
talent-dev216 <talent.dev0216@gmail.com>
2022-10-26 16:48:45 +0300

    preparing an example "points redemption" #ONBOARDING-15

---
+++ Examples/points_redemption.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Points redemption
+ */
+
+use CrmCareCloud\Webservice\RestApi\Client\ApiException;
+use CrmCareCloud\Webservice\RestApi\Client\Model\ActionsReducepointsBody;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\CareCloud;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\Config;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\Data\AuthTypes;
+
+require_once '../vendor/autoload.php';
+
+$project_uri = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
+$login = 'login';
+$password = 'password';
+$external_app_id = 'application_id';
+$auth_type = AuthTypes::BEARER_AUTH;
+// Or if using basic auth, just change the AuthType to Basic Auth
+// $authType      = AuthTypes::BASIC_AUTH;
+
+$config = new Config($project_uri, $login, $password, $external_app_id, $auth_type);
+
+$care_cloud = new CareCloud($config);
+
+// Set Header parameter Accept-Language
+$accept_language = 'en'; //	string | The unique id of the language code by ISO 639-1 Default: cs, en-gb;q=0.8
+
+// Set the request body
+$body = new ActionsReducepointsBody();
+$body->setAmount(17); // float | Number of redeemed points
+$body->setCustomerId('8ea2591121e636086a4a9c0992'); // string | The unique id of the customer
+$body->setPartnerId(null); // string | The unique id of the partner (optional)
+
+// Call endpoint and post data
+try
+{
+    $care_cloud->pointsApi()->postPointsRedemption($body, $accept_language);
+}
+catch(ApiException $e)
+{
+    die(var_dump($e->getResponseBody() ?: $e->getMessage()));
+}
