7cb84943b9dcc45f739ffa5d6c689b2161a683c7
talent-dev216 <talent.dev0216@gmail.com>
2022-10-28 14:39:57 +0300

    preparing an example "get a reservable product" #ONBOARDING-20

---
+++ Examples/get_a_reservable_product.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Get a reservable product
+ */
+
+use CrmCareCloud\Webservice\RestApi\Client\ApiException;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\CareCloud;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\Config;
+use CrmCareCloud\Webservice\RestApi\Client\SDK\Data\AuthTypes;
+
+require_once '../vendor/autoload.php';
+
+$project_uri = 'https://yourapiurl.com/webservice/rest-api/enterprise-interface/v1.0';
+$login = 'login';
+$password = 'password';
+$external_app_id = 'application_id';
+$auth_type = AuthTypes::BEARER_AUTH;
+// Or if using basic auth, just change the AuthType to Basic Auth
+// $authType      = AuthTypes::BASIC_AUTH;
+
+$config = new Config($project_uri, $login, $password, $external_app_id, $auth_type);
+
+$care_cloud = new CareCloud($config);
+
+// Set Header parameter Accept-Language
+$accept_language = 'en'; //	string | The unique id of the language code by ISO 639-1 Default: cs, en-gb;q=0.8
+
+// Set path parameters
+$reservable_product_id = '89bdacbffb5c82f2c52cb9ae8f'; // string | The unique id f the reservable product
+
+// Call endpoint and get data
+try
+{
+    $get_reservable_product = $care_cloud->reservableProductsApi()->getReservableProduct($reservable_product_id, $accept_language);
+    $reservable_product = $get_reservable_product->getData();
+}
+catch(ApiException $e)
+{
+    die(var_dump($e->getResponseBody() ?: $e->getMessage()));
+}
