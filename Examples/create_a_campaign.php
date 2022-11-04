1caec07b4f18195a53cc9e4253d026e85701ffa1
talent-dev216 <talent.dev0216@gmail.com>
2022-11-04 13:58:37 +0300

    removing the odd comments from examples #ONBOARDING-7

--- Examples/create_a_campaign.php
+++ Examples/create_a_campaign.php
@@ -17,8 +17,6 @@ $login = 'login';
 $password = 'password';
 $external_app_id = 'application_id';
 $auth_type = AuthTypes::BEARER_AUTH;
-// Or if using basic auth, just change the AuthType to Basic Auth
-// $authType      = AuthTypes::BASIC_AUTH;
 
 $config = new Config($project_uri, $login, $password, $external_app_id, $auth_type);
 
