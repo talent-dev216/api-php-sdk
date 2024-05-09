9cde3ff0ba0920844e2f493edb8a49c1de2dd572
talent-dev216 <talent.dev0216@gmail.com>
2024-05-09 07:56:44 +0200

    Added the Rector library to make it easier to upgrade API SDK to a higher version of PHP and increased version of PHP #RCC-3292

---
+++ rector.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Set\ValueObject\LevelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        //__DIR__ . '/Examples',
+        __DIR__ . '/Tests',
+        __DIR__ . '/src',
+    ])
+    ->withSkip([
+        ClassPropertyAssignToConstructorPromotionRector::class
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withSets([
+        LevelSetList::UP_TO_PHP_82,
+    ]);
